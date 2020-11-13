0b30982f2303305559c63c4c8dc7e45fd71726f7
William Truppi <williamt@hotmail.it>
2020-11-13 14:55:12 +0100

    Acquisizione dati in input completata

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/* Il programma dovrà chiedere all'utente 
+il numero di chilometri che vuole percorrere e 
+l'età del passeggero. */
+
+var userKilometers = Number(prompt("Quanti kilometri ha intezione di percorrere?"));
+var userAge = Number(prompt("Inserire la Sua età"))
+
+console.log(userKilometers, userAge);
