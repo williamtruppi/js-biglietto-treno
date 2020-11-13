d5bdd8838b12c11d2a4f7c11a843b5a0c76ca752
William Truppi <williamt@hotmail.it>
2020-11-13 15:28:11 +0100

    esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,19 @@ il numero di chilometri che vuole percorrere e
 l'età del passeggero. */
 
 var userKilometers = Number(prompt("Quanti kilometri ha intezione di percorrere?"));
-var userAge = Number(prompt("Inserire la Sua età"))
+var userAge = Number(prompt("Inserire la Sua età"));
 
-console.log(userKilometers, userAge);
+//console.log(userKilometers, userAge);
+
+var finalPrice;
+
+if (userAge < 18) {
+  finalPrice = (userKilometers * 0.21) - ((userKilometers * 0.21) * 0.20);
+  alert("Lei aderisce allo sconto \"MINORENNI\", pertanto il costo del viaggio è di €. " + finalPrice);
+} else if (userAge >= 65) {
+  finalPrice = (userKilometers * 0.21) - ((userKilometers * 0.21) * 0.40);
+  alert("Lei aderisce allo sconto \"OVER 65\", pertanto il costo del viaggio è di €. " + finalPrice);
+} else {
+  finalPrice = userKilometers * 0.21;
+  alert("Il costo del biglietto è di €." + finalPrice);
+}
